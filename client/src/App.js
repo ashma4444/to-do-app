e7d39b65457931870516154f4816901c6ea98d3a
ashma4444 <ashma20570218@gmail.com>
2023-09-03 16:04:50 +0545

    learned react component

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Car from "./Car";
+
+function App() {
+  return (
+    // fragments
+    <>
+      <div className="App">hello world</div>
+      <Car />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Car.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Car() {
+  return <div>Car. k cha?</div>;
+}
+
+export default Car;

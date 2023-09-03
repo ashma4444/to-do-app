f7f2546f9e0c971841c3a9f13c2afd0dd6f67504
ashma4444 <ashma20570218@gmail.com>
2023-09-03 18:00:34 +0545

    learned list rendering

--- client/src/Car.js
+++ client/src/Car.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function Car(props) {
   // JS logic
+  const cars = ["BMW", "Tesla"];
   const shoot = (name) => {
     alert(`Shoot... by ${name}`);
   };
@@ -10,10 +11,16 @@ function Car(props) {
   return (
     <>
       <div>Car is of color {props.color}</div>
-      <input id="name" />
+      <input id="name" type="text" />
       <button onClick={() => shoot(document.getElementById("name").value)}>
         Click me
       </button>
+
+      <ol>
+        {cars.map((car, index) => {
+          return <li key={index}>{car}</li>;
+        })}
+      </ol>
     </>
   );
 }
